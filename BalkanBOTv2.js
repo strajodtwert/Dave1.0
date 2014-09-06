2095fce96e1d5e8e37aab112e2abcbfbd140b5ff
MrAjdin <alicajdin_97_@hotmail.com>
2014-09-06 16:01:02 +0200

    Update BalkanBOTv2.js

--- BalkanBOTv2.js
+++ BalkanBOTv2.js
@@ -2035,7 +2035,7 @@ if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0)
 if (!bBot.commands.executable(this.rank, chat)) return void (0);
 else {
 var link = "http://ask.fm/BalkanParty12";
-API.sendChat(subChat(bBot.chat.ask, {link: link, name: chat.un}));
+API.sendChat(subChat(bBot.chat.ask, {link: link}));
 }
 }
 },
