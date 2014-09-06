1f1b93989bef82a7bfddb85d27bfa1445b59bc0f
MrAjdin <alicajdin_97_@hotmail.com>
2014-09-06 16:10:18 +0200

    Update BalkanBOTv2.js

--- BalkanBOTv2.js
+++ BalkanBOTv2.js
@@ -2034,7 +2034,7 @@ functionality: function (chat, cmd) {
 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
 if (!bBot.commands.executable(this.rank, chat)) return void (0);
 else {
-API.sendChat("If you got any question feel free to ask us here: http://ask.fm/BalkanParty12");
+API.sendChat("/me If you got any question feel free to ask us here: http://ask.fm/BalkanParty12");
 }
 }
 },
