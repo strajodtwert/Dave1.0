14cefc8bbec254dd2a949ad7cfd2f2e51cdf4a87
MrAjdin <alicajdin_97_@hotmail.com>
2014-09-29 11:35:20 +0200

    Update BalkanBOT.js

--- Bot/BalkanBOT.js
+++ Bot/BalkanBOT.js
@@ -2056,6 +2056,18 @@ API.sendChat(subChat(bBot.chat.ahmed, {name: chat.un}));
 }
 }
 },
+songunbanCommand: {
+command: 'songunban',
+rank: 'user',
+type: 'exact',
+functionality: function (chat, cmd) {
+if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+if (!bBot.commands.executable(this.rank, chat)) return void (0);
+else {
+API.sendChat(subChat(bBot.chat.songunban, {name: chat.un}));
+}
+}
+},
 
             joinCommand: {
                 command: 'join',
