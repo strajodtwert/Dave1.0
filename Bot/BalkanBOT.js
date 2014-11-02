58efd39f7e9a0bf99676c3649373307137967337
MrAjdin <alicajdin_97_@hotmail.com>
2014-11-02 17:08:18 +0100

    Update BalkanBOT.js

--- Bot/BalkanBOT.js
+++ Bot/BalkanBOT.js
@@ -2079,7 +2079,7 @@ functionality: function (chat, cmd) {
 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
 if (!bBot.commands.executable(this.rank, chat)) return void (0);
 else {
-API.sendChat(subChat(bBot.chat.botwoot, {name: chat.un}));
+API.sendChat(subChat(bBot.chat.botwoot));
 $("#woot").click();
 API.on(API.ADVANCE, autowoot);
 function autowoot(){ $("#woot").click(); }
