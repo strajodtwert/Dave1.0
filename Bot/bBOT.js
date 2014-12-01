34c99d516ced649fb840f88e0e76e44bd5b71cce
MrAjdin <alicajdin_97_@hotmail.com>
2014-12-01 10:52:38 +0100

    Update bBOT.js

--- Bot/bBOT.js
+++ Bot/bBOT.js
@@ -1967,6 +1967,19 @@ API.chatLog("[ BalkanBOT ] LAST UPDATED: 02.11.2014", true);
                     }
                 }
             },
+            
+            roomhelpCommand: {
+                command: 'roomhelp',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!bBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                    	API.sendChat(subChat(bBot.chat.roomhelp, {name: chat.un}));
+                    }
+                }
+            },
              
             eldoxCommand: {
 command: 'eldox',
