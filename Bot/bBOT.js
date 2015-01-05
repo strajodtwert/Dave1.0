62b6a64e2173f9ed968eb7c13a2cbd439f287c15
MrAjdin <alicajdin_97_@hotmail.com>
2015-01-05 23:16:34 +0100

    Update bBOT.js

--- Bot/bBOT.js
+++ Bot/bBOT.js
@@ -2739,7 +2739,7 @@ function autowoot(){ $("#woot").click(); }
                     if (!bBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         if (typeof bBot.settings.opLink === "string")
-                            return API.sendChat(subChat(bBot.chat.oplist, {link: bBot.settings.opLink}));
+                            return API.sendChat(subChat(bBot.chat.oplist, {name: chat.un, link: bBot.settings.opLink}));
                     }
                 }
             },
