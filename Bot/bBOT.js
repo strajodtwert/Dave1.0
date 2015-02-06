db8d7e5705c081fe2c564aeee70619cf1cb50b7b
Ajdin Alic <alicajdin_97_@hotmail.com>
2015-02-06 19:05:31 +0100

    Update bBOT.js

--- Bot/bBOT.js
+++ Bot/bBOT.js
@@ -2207,18 +2207,6 @@ API.sendChat(subChat(bBot.chat.ahmed, {name: chat.un}));
 }
 }
 },
-mickoCommand: {
-command: 'micko',
-rank: 'user',
-type: 'exact',
-functionality: function (chat, cmd) {
-if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-if (!bBot.commands.executable(this.rank, chat)) return void (0);
-else {
-API.sendChat(subChat(bBot.chat.micko, {name: chat.un}));
-}
-}
-},
 songunbanCommand: {
 command: 'songunban',
 rank: 'user',
